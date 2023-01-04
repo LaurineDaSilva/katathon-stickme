de45362aa176bf5cc8901c6f4b2c216ebc751909
LaurineDS <laurine.dasilva0@gmail.com>
2023-01-04 15:20:51 +0100

    Back-end: create DTO for sticker creation

---
+++ katathon-stickme-back/src/main/java/co/simplon/stickme/dtos/StickerCreateDto.java
@@ -0,0 +1,72 @@
+package co.simplon.stickme.dtos;
+
+public class StickerCreateDto {
+
+    private String name;
+    private String imageUrl;
+    private String Description;
+    private Long chosenAspectId;
+    private Long chosenSizeId;
+    private float price;
+
+    public StickerCreateDto() {
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public String getImageUrl() {
+	return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+	this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+	return Description;
+    }
+
+    public void setDescription(String description) {
+	Description = description;
+    }
+
+    public Long getChosenAspectId() {
+	return chosenAspectId;
+    }
+
+    public void setChosenAspectId(Long chosenAspectId) {
+	this.chosenAspectId = chosenAspectId;
+    }
+
+    public Long getChosenSizeId() {
+	return chosenSizeId;
+    }
+
+    public void setChosenSizeId(Long chosenSizeId) {
+	this.chosenSizeId = chosenSizeId;
+    }
+
+    public float getPrice() {
+	return price;
+    }
+
+    public void setPrice(float price) {
+	this.price = price;
+    }
+
+    @Override
+    public String toString() {
+	return "{name=" + name + ", imageUrl=" + imageUrl
+		+ ", Description=" + Description
+		+ ", chosenAspectId=" + chosenAspectId
+		+ ", chosenSizeId=" + chosenSizeId
+		+ ", price=" + price + "}";
+    }
+
+}
