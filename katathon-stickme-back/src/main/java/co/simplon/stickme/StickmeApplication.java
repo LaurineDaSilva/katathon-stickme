569acdee3a24d78a6cf8f2f62dc2f617fdf7695e
LaurineDS <laurine.dasilva0@gmail.com>
2023-01-04 14:54:41 +0100

    creates maven project for back-end

---
+++ katathon-stickme-back/src/main/java/co/simplon/stickme/StickmeApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.stickme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickmeApplication.class, args);
+	}
+
+}
---
+++ katathon-stickme-back/src/test/java/co/simplon/stickme/StickmeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.stickme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
